d0ab2e2697645178787e80b9acb5f61706f55cea
Musa Dabev <musa_dv@abv.bg>
2023-02-05 14:23:57 +0200

    sideBar implemented

---
+++ front-end/src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import NavBar from "../../components/NavBar/NavBar";
+import SideBar from "../../components/SideBar/SideBar";
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+      <SideBar />
+    </>
+  );
+}
+
+export default Home;
