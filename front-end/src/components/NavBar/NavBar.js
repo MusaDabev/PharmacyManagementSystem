871dc89ade561c04c62de1ecd152d385f9f20d2a
Musa Dabev <musa_dv@abv.bg>
2023-02-05 14:22:41 +0200

    navBar added

---
+++ front-end/src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav class="navbar bg-light">
+      <div class="container-fluid">
+        <p class="navbar-brand">
+          PharmacyMS
+        </p>
+        <p>Добре дошли!</p>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
