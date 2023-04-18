b73ffa10232e6e583095ec1513568c6948811509
Musa Dabev <musa_dv@abv.bg>
2023-04-18 15:25:23 +0300

    image added to Add supplier

--- front-end/src/Pages/AddSupplier/AddSupplier.js
+++ front-end/src/Pages/AddSupplier/AddSupplier.js
@@ -1,12 +1,16 @@
 import React from "react";
 import SideBar from "../../components/SideBar/SideBar";
 import AddSupplierForm from "../../components/AddSupplierForm/AddSupplierForm";
+import SupplierImg from "../../assets/pictures/icons/supplier.svg";
 
 function AddSupplier() {
   return (
     <div className="d-flex">
       <SideBar />
-      <AddSupplierForm />
+      <div className="w-100 d-flex flex-column mt-2">
+          <img className="mx-auto" src={SupplierImg} width="100%" height="60%"></img>
+          <AddSupplierForm />
+        </div>
     </div>
   );
 }
