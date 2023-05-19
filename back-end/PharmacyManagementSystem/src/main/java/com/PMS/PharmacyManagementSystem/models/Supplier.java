25460c4a82c644e8163524aa240f6c70905a5b01
Musa Dabev <musa_dv@abv.bg>
2023-05-19 11:06:46 +0300

    add relationships

--- back-end/PharmacyManagementSystem/src/main/java/com/PMS/PharmacyManagementSystem/models/Supplier.java
+++ back-end/PharmacyManagementSystem/src/main/java/com/PMS/PharmacyManagementSystem/models/Supplier.java
@@ -11,7 +11,13 @@ public class Supplier {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private String phone;
+
+    @Column
+    private String website;
 }
