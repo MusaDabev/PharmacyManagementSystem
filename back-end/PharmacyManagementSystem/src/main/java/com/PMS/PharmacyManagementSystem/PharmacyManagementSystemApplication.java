e66dc52731531d7348ff124c09456f7d2b4b9a6c
Musa Dabev <musa_dv@abv.bg>
2023-05-19 10:56:08 +0300

    add slawagger

--- back-end/PharmacyManagementSystem/src/main/java/com/PMS/PharmacyManagementSystem/PharmacyManagementSystemApplication.java
+++ back-end/PharmacyManagementSystem/src/main/java/com/PMS/PharmacyManagementSystem/PharmacyManagementSystemApplication.java
@@ -2,8 +2,15 @@ package com.PMS.PharmacyManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PharmacyManagementSystemApplication {
 
 	public static void main(String[] args) {
