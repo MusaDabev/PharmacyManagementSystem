1eb274900fe2db832cb5d9f7c0289013182951bb
Musa Dabev <musa_dv@abv.bg>
2023-06-03 00:00:05 +0300

    add swagger configuration

--- back-end/PharmacyManagementSystem/src/main/java/com/PMS/PharmacyManagementSystem/PharmacyManagementSystemApplication.java
+++ back-end/PharmacyManagementSystem/src/main/java/com/PMS/PharmacyManagementSystem/PharmacyManagementSystemApplication.java
@@ -3,7 +3,11 @@ package com.PMS.PharmacyManagementSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -17,4 +21,21 @@ public class PharmacyManagementSystemApplication {
 		SpringApplication.run(PharmacyManagementSystemApplication.class, args);
 	}
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.PMS.PharmacyManagementSystem"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("PharmaRx")
+				.description("PharmaRx API")
+				.version("1.0.0")
+				.build();
+	}
 }
